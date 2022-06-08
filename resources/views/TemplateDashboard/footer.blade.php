97c3d08afafbc813da5db9c3c891e9923f09ad37
yudistira-rk <yudistiraeducation100@gmail.com>
2022-06-08 16:46:42 +0700

    membuat dashboard admin

---
+++ resources/views/TemplateDashboard/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      Anything you want
+    </div>
+    <!-- Default to the left -->
+    <strong>Inovis 2022 
+         <a href="https://academy.inovis.id/" target="blank"> SIPO</a>.</strong> Sistem Informasi Posyandu
+  </footer>
